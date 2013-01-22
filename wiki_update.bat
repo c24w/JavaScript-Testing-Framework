b98a939871974f07cd6d24c504fb6321d88bd8b8
Chris Watson <chris.watson@7digital.com>
2013-01-22 13:19:57 +0000

    Remove submodule script - run manually when first cloned

--- wiki_update.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: submodules behave strangely, may want to revert to wiki_setup.bat with wiki_pull.bat
-call git submodule init
-call git submodule update
-cd %~dp0wiki
-call git checkout master
